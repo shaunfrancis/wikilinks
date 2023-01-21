01d6b0fc8a87c3bf26679c96d10eeecfac70f60b
shaunfrancis <shaunfrancis@gmx.com>
2023-01-21 19:59:15 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include '../scripts/ini.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles/app.css" />
+    <script src="https://unpkg.com/react@18/umd/react.production.min.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js" crossorigin></script>
+    <script type="text/javascript" src="app.js"></script>
+</head>
+<body>
+    <?php include '../components/header.php'; ?>
+    Hello, world!
+</body>
+</html>

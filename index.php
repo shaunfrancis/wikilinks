89aeb0f5fc9ce7ef50c9bb3df7289884df7bb24f
shaunfrancis <shaunfrancis@gmx.com>
2023-01-22 22:52:57 +0700

    Initial structure and logo

--- index.php
+++ index.php
@@ -3,12 +3,42 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="styles/app.css" />
-    <script src="https://unpkg.com/react@18/umd/react.production.min.js" crossorigin></script>
-    <script src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js" crossorigin></script>
     <script type="text/javascript" src="app.js"></script>
+    <title>Wikilinks - The Wikipedia Racing Game! - Tennessine</title>
 </head>
 <body>
-    <?php include '../components/header.php'; ?>
-    Hello, world!
+    <?php $external_header = TRUE; include '../components/header.php'; ?>
+
+    <main class="page visible">
+        <aside>
+            <img id="logo" alt="Wikilinks" src="images/logo.png" />
+            <div id="mode-container">
+                <div class="card">
+                    <h2>Single Player</h2>
+                </div>
+                <div class="card">
+                    <h2>Multiplayer</h2>
+                </div>
+                <div class="card">
+                    <h2>Tutorial</h2>
+                </div>
+            </div>
+        </aside>
+        <div class="content">
+            <section>
+                <h1>Today's Game</h1>
+            </section>
+        </div>
+    </main>
+
+    <section id="game" class="page visible">
+        <aside>
+            game aside
+        </aside>
+        <section id="game-content" class="content">
+            game-content
+        </section>
+    </section>
+
 </body>
 </html>
